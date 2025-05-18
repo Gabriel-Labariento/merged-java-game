696242ed73c8f7dadbbdb296981434a1234b3244
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-18 23:39:06 +0800

    Javadoc FeralDog

--- FeralDog.java
+++ FeralDog.java
@@ -3,6 +3,27 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The FeralDog extends Enemy. It is one of the bosses in the game.
+        It appears at the end of level 2. Its primary behavior is follow
+        and then slash at the Player when near. It can create minions and
+        apply buff to them.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of my program.
+**/
+
 public class FeralDog extends Enemy {
     private int SPAWN_COOLDOWN = 5000;
     private int ATTACK_COOLDOWN = 4000;
@@ -14,10 +35,18 @@ public class FeralDog extends Enemy {
     private Phase currentPhase;
     private State currentState;
 
+    /**
+     * Call the static setSprites() method
+     */
     static {
         setSprites();
     }
 
+    /**
+     * Creates a FeralDog instance with appropriate fields
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public FeralDog(int x, int y) {
         isBoss = true;
         id = enemyCount++;
@@ -38,6 +67,9 @@ public class FeralDog extends Enemy {
         isBoss = true;
     }
 
+    /**
+     * Set the sprite images to the class and not the instance
+     */
     private static void setSprites() {
         try {
             BufferedImage left0 = ImageIO.read(FeralDog.class.getResourceAsStream("resources/Sprites/FeralDog/feralDog_left0.png"));
@@ -82,8 +114,9 @@ public class FeralDog extends Enemy {
         Player pursued = scanForPlayer(gsm);
 
         switch (currentPhase) {
+
             case NORMAL:
-                if (hitPoints <= maxHealth / 2) currentPhase = Phase.BUFFED;
+                if (hitPoints <= maxHealth / 2) currentPhase = Phase.BUFFED; // If health is below half, switch phase
                 else {
                     if (pursued == null) return;
 
@@ -112,6 +145,7 @@ public class FeralDog extends Enemy {
 
                 handleStateBehavior(now, pursued, gsm);
                 
+                // Loop through created enemies and apply buff to them
                 for (Entity e : gsm.getEntities()) {
                     if (e instanceof Enemy enemy && enemy != this) {
                         enemy.applyBuff();
@@ -127,38 +161,24 @@ public class FeralDog extends Enemy {
         matchHitBoxBounds();
     }
 
-    @Override
-    public Player scanForPlayer(ServerMaster gsm){
-        final int scanRadius = GameCanvas.TILESIZE * 10; // Larger scan radius
-        Player closestPlayer = null;
-        double minDistance = Integer.MAX_VALUE;
-
-        for (Entity e : gsm.getEntities()) {
-            if (e instanceof Player player) {
-                if (this.getCurrentRoom() != player.getCurrentRoom()) continue; 
-                // Get the center distance between the player and the entity
-                double distanceSquared = 
-                    (Math.pow(getCenterX() - e.getCenterX(), 2) + Math.pow(getCenterY() - e.getCenterY(), 2));
-                
-                if ( (distanceSquared <= scanRadius * scanRadius) && (distanceSquared < minDistance)) {
-                    closestPlayer = player;
-                    minDistance = distanceSquared;
-                }
-            }
-        }
-        return closestPlayer;
-    }
-
+    /**
+     * Handles the behavior of the instance based on its currentState
+     * @param now the time when the method was invoked
+     * @param pursued the target Player
+     * @param gsm the ServerMaster instance containing the entities ArrayList 
+     */
     private void handleStateBehavior(long now, Player pursued, ServerMaster gsm){
         switch (currentState) {
             case IDLE:
+                // Switch to attack state after cool down
                 if (now - lastAttackTime > ATTACK_COOLDOWN){
                     currentState = State.ATTACK;
                     lastAttackTime = now;
                 } else currentState = State.PURSUE;
                 break;
-
+            
             case PURSUE:
+                // If within range, telegraph before attack
                 if (getSquaredDistanceBetween(this, pursued) <= ATTACK_RANGE * ATTACK_RANGE) {
                     currentState = State.IDLE;
                 } else pursuePlayer(pursued);
@@ -166,29 +186,35 @@ public class FeralDog extends Enemy {
 
             case ATTACK:
                 createSlashAttack(gsm, pursued, null);
-                currSprite = (pursued.getWorldX() > worldX) ? 5 : 2;
+                currSprite = (pursued.getWorldX() > worldX) ? 5 : 2; // Attack Sprites
                 lastAttackTime = now;
                 currentState = State.PURSUE;
                 break;
         }
     }
 
+    /**
+     * Handles the sprite to be shown based on Phase (Normal / Buffed)
+     * and position in relation to target Player
+     * @param now the time when the method was invoked
+     * @param pursued the targeted Player
+     */
     private void updateWalkFrame(long now, Player pursued){
         // Sprite walk update
         if (now - lastSpriteUpdate > SPRITE_FRAME_DURATION) {
             if (currentPhase == Phase.NORMAL) {
-                if (worldX > pursued.getWorldX()) { // LEFT
+                if (worldX > pursued.getWorldX()) {                     // LEFT
                 currSprite++;
                 if (currSprite > 2) currSprite = 0;
-                } else if (worldX < pursued.getWorldX()) { // RIGHT
+                } else if (worldX < pursued.getWorldX()) {              // RIGHT
                     currSprite++;
                     if (currSprite < 3 || currSprite > 5) currSprite = 3;
                 } lastSpriteUpdate = now;
             } else { // BUFFED SPRITES
-                if (worldX > pursued.getWorldX()) { // LEFT
+                if (worldX > pursued.getWorldX()) {                     // LEFT
                     if (currSprite < 6 || currSprite > 8) currSprite = 6;
                     else currSprite = (currSprite + 1 > 8) ? 6 : currSprite + 1;
-                } else if (worldX < pursued.getWorldX()) { // RIGHT
+                } else if (worldX < pursued.getWorldX()) {              // RIGHT
                     if (currSprite < 9 || currSprite > 11) currSprite = 9;
                     else currSprite = (currSprite + 1 > 11) ? 9 : currSprite + 1;
                 } lastSpriteUpdate = now;
