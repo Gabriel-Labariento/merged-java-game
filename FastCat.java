b35cf39c7ce50dcf56b1a3abfae543d8c7823c18
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-18 23:31:10 +0800

    FastCat javadoc

--- FastCat.java
+++ FastCat.java
@@ -3,13 +3,42 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The FastCat extends the Player class. It is one of three
+        playable Player classes. This Player supports was designed
+        for a melee attack with faster movement.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of my program.
+**/
+
 public class FastCat extends Player{
     private static BufferedImage[] sprites;
 
+    /**
+     * Calls the static setSprites() method
+     */
     static {
         setSprites();
     }
 
+    /**
+     * Creates a FastCat instance with appropriate fields
+     * @param cid the clientId of the Player creating the instance
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public FastCat(int cid, int x, int y){
         this.clientId = cid;
         identifier = NetworkProtocol.FASTCAT;
@@ -36,6 +65,9 @@ public class FastCat extends Player{
         g2d.drawImage(sprites[currSprite], xOffset, yOffset, width, height, null);
     }
 
+    /**
+     * Set the sprite images to the class
+     */
     private static void setSprites() {
         try {
             BufferedImage left0 = ImageIO.read(FastCat.class.getResourceAsStream("resources/Sprites/FastCat/left0.png"));
@@ -51,7 +83,7 @@ public class FastCat extends Player{
             sprites = new BufferedImage[] {left0, left1, left2, right0, right1, right2, attack0, attack1, attack2, death};
 
         } catch (IOException e) {
-            System.out.println("Exception in Rat setSprites()" + e);
+            System.out.println("Exception in FastCat setSprites()" + e);
         }
     }
 
