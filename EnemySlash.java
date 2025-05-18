1aa1a23a174e3ed24eba8ee280df0c950e58743d
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-18 22:58:17 +0800

    Javadoc EnemySlash

--- EnemySlash.java
+++ EnemySlash.java
@@ -3,11 +3,34 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The EnemySlash class extends Attack. It creates 
+        a wide attack attached to the owner. It damages
+        Players.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of my program.
+**/
+
 public class EnemySlash extends Attack{
     public static final int HEIGHT = 56;
     public static final int WIDTH = 56;
     private static BufferedImage sprite;
 
+    /**
+     * Set sprite images to the class, not the instances
+     */
     static {
         try {
             BufferedImage img = ImageIO.read(EnemySlash.class.getResourceAsStream("resources/Sprites/SharedEnemy/enemyslash.png"));
@@ -17,6 +40,12 @@ public class EnemySlash extends Attack{
         }
     }
 
+    /**
+     * Creates an EnemySlash instance with appropriate fields
+     * @param owner the Entity creating the attack, to which it is attached to
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public EnemySlash(Entity owner, int x, int y){
         attackNum++;
         id = attackNum;
