c90c8f8cc82dfa8138cf82385b5f1760475c3a29
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 23:03:50 +0800

    javadoc premiumcatfood

--- PremiumCatFood.java
+++ PremiumCatFood.java
@@ -3,8 +3,29 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The PremiumCatFood class extends Item. It increases the owner's 
+        damage by 1.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 public class PremiumCatFood extends Item {
     public static BufferedImage sprite;
+
+    // Sets the sprite of the item to the class
     static {
         try {
             sprite = ImageIO.read(PremiumCatFood.class.getResourceAsStream("resources/Sprites/Items/premiumcatfood.png"));
@@ -13,6 +34,11 @@ public class PremiumCatFood extends Item {
         }
     }
     
+    /**
+     * Creates a new PremiumCatFood instance with appropriate fields
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public PremiumCatFood(int x, int y){
         identifier = NetworkProtocol.PREMIUMCATFOOD;
         worldX = x;
