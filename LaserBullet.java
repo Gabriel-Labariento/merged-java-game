4effbad7c26e64a0e9436e5d052e88ae901cf571
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 21:21:57 +0800

    Javadoc LaserBullet

--- LaserBullet.java
+++ LaserBullet.java
@@ -3,12 +3,32 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The LaserBullet class extends Attack. It is a fast moving
+        bullet that travels from an Entity origin outward using vectors.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
  public class LaserBullet extends Attack {
         public static final int HEIGHT = 16;
         public static final int WIDTH = 16;
         double normalizedX, normalizedY;
         private static BufferedImage sprite;
 
+        // Sets sprite
         static {
             try {
                 BufferedImage img = ImageIO.read(LaserBullet.class.getResourceAsStream("resources/Sprites/Attacks/laserbullet.png"));
@@ -17,7 +37,15 @@ import javax.imageio.ImageIO;
                 System.out.println("Exception in setSprites()" + e);
             }
         }
-
+        
+        /**
+         * Creates a LaserBullet instance with appropriate fields
+         * @param owner the Entity spawning the Attack
+         * @param x the spawn x-coordinate
+         * @param y the spawn y-coordinate
+         * @param nX the normalized x coordinate of the direction to travel to
+         * @param nY the normalized y coordinate of the direction to travel to
+         */
         public LaserBullet(Entity owner, int x, int y, double nX, double nY){
             attackNum++;
             id = attackNum;
@@ -59,6 +87,10 @@ import javax.imageio.ImageIO;
             hitBoxBounds[3] = worldX + width;
         }
 
+        /**
+         * Moves the bullet along a trajectory based on the speed and the direction
+         * provided by normalizedX and normalizedY
+         */
         private void moveBullet(){
             worldX += speed * normalizedX;
             worldY += speed * normalizedY;
