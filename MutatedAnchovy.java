0e0712243e697bc471df4d7fb299b850d316e37a
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 21:54:48 +0800

    Javadoc MutatedAnchovy

--- MutatedAnchovy.java
+++ MutatedAnchovy.java
@@ -3,13 +3,40 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The MutatedAnchovy class extends Enemy. It appears in level
+        7 of the game. It follows the player and runs from it between 
+        attacks to refresh cooldown.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
+
 public class MutatedAnchovy extends Enemy{
     private static BufferedImage[] sprites;
 
+    // Calls the static setSprites() method
     static {
         setSprites();
     }
 
+    /**
+     * Creates a MutatedAnchovy instance with appropriate fields
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public MutatedAnchovy(int x, int y) {
         id = enemyCount++;
         lastSpriteUpdate = 0;
@@ -30,7 +57,10 @@ public class MutatedAnchovy extends Enemy{
         
     }
 
-     private static void setSprites() {
+    /**
+     * Sets the sprite images to the class and not the instance
+     */
+    private static void setSprites() {
         try {
             BufferedImage left0 = ImageIO.read(MutatedAnchovy.class.getResourceAsStream("resources/Sprites/MutatedAnchovy/left0.png"));
             BufferedImage right0 = ImageIO.read(MutatedAnchovy.class.getResourceAsStream("resources/Sprites/MutatedAnchovy/right0.png"));
