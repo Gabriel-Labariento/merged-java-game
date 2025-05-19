c16010a231e5a5e15882b5f2d3245b30097b823c
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 11:15:26 +0800

    Javadoc GunCat

--- GunCat.java
+++ GunCat.java
@@ -3,14 +3,43 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The GunCat extends the Player class. It is one of three
+        playable Player classes. This Player supports was designed
+        for ranged attacks.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
+
 public class GunCat extends Player{
     private static BufferedImage[] sprites;
 
+    /**
+     * Calls the static setSprites() method
+     */
     static {
         setSprites();
     }
     
-
+    /**
+     * Creates a new GunCat instance with appropriate fields
+     * @param cid the client Id of the Player creating this class
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public GunCat(int cid, int x, int y){
         this.clientId = cid;
         identifier = NetworkProtocol.GUNCAT;
@@ -36,7 +65,9 @@ public class GunCat extends Player{
         g2d.drawImage(sprites[currSprite], xOffset, yOffset, width, height, null);
     }
 
-    
+    /**
+     * Set the sprite images to the class and not the instance
+     */
     private static void setSprites() {
         try {
             BufferedImage left0 = ImageIO.read(GunCat.class.getResourceAsStream("resources/Sprites/GunCat/left0.png"));
