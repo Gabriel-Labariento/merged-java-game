9c5bc027f67cab48991e08bbd93590783b18d3d4
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 23:05:37 +0800

    javadoc PringlesCan

--- PringlesCan.java
+++ PringlesCan.java
@@ -3,8 +3,31 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The PringlesCan class extends Item. It buffs up the Player's defense
+        but increases its attack cooldown time and decreases the owner's
+        damage.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
+
 public class PringlesCan extends Item {
     public static BufferedImage sprite;
+
+    // Set sprites to the class
     static {
         try {
             sprite = ImageIO.read(PringlesCan.class.getResourceAsStream("resources/Sprites/Items/pringlescan.png"));
@@ -13,6 +36,11 @@ public class PringlesCan extends Item {
         }
     }
     
+    /**
+     * Creates a PringlesCan instance with appropriate fields
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public PringlesCan(int x, int y){
         identifier = NetworkProtocol.PRINGLESCAN;
         worldX = x;
