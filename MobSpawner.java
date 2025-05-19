0f796ecd77d0b915e752cf90ae842afdb72c6aea
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 21:49:36 +0800

    Javadoc MobSpawner

--- MobSpawner.java
+++ MobSpawner.java
@@ -1,6 +1,28 @@
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
+/**     
+        The MobSpawner class handles enemy spawning within rooms.
+        It controls enemy spawn rates, spawn locations, and spawn
+        quantities based on the current game level and difficulty.
+        Normal enemies are spawned randomly inside the room. In boss
+        rooms, the boss is spawned at the center.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 public class MobSpawner {
     
     private int spawnX, spawnY; // Where the mob will spawn in the room, x = (5 => 40), y = (5 => 28) 
@@ -43,6 +65,11 @@ public class MobSpawner {
 
     private ScheduledExecutorService spawnMobsScheduler;
 
+    /**
+     * Creates a new MobSpawner with specified level and difficulty
+     * @param level the room level where the MobSpawner is located
+     * @param difficulty the difficulty of the room where MobSpawner is located
+     */
     public MobSpawner(int level, int difficulty){
         this.level = level;
         this.difficulty = difficulty;
@@ -58,6 +85,10 @@ public class MobSpawner {
         boss = null;
     }
 
+    /**
+     * Starts a scheduled task that spawns enemies at spawnRate intervals
+     * until the maximum number of spawnable enemies at that room has been reached.
+     */
     public void spawn() {
 
         Runnable enemySpawnThread = new Runnable() {
@@ -89,6 +120,9 @@ public class MobSpawner {
         spawnMobsScheduler.scheduleAtFixedRate(enemySpawnThread, INITIALSPAWNDELAY, spawnRate, TimeUnit.SECONDS);
     }
 
+    /**
+     * Stops the scheduler responsible for spawning enemies
+     */
     public void stopSpawn() {
         try {
             spawnMobsScheduler.shutdownNow();    
@@ -98,6 +132,10 @@ public class MobSpawner {
         
     }
 
+    /**
+     * Gets the number of enemies who have died after spawning
+     * @return the int number of enemies spawned who died
+     */
     public int getKilledCount(){
         int killed = 0;
 
@@ -115,10 +153,21 @@ public class MobSpawner {
         return (getKilledCount() >= maxSpawned);
     }
 
+    /**
+     * Gets the value of the isSpawning field
+     * @return true if isSpawning, false otherwise
+     */
     public boolean isSpawning() {
         return isSpawning;
     }
     
+    /**
+     * Creates an instance of the enemy corresponding to the provided name
+     * @param name the name of the enemy to return
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     * @return a new instance of the enemy corresponding to the passed name
+     */
     private Enemy createEnemy(String name, int x, int y) {
         switch (name) {
             case "AdultCat":
@@ -171,14 +220,27 @@ public class MobSpawner {
         }
     }
 
+    /**
+     * Gets the value of the inBossRoom field
+     * @return true if inBossRoom is true, false otherwise
+     */
     public boolean isInBossRoom() {
         return inBossRoom;
     }
 
+    /**
+     * Sets the value of inBossRoom to the passed argument
+     * @param inBossRoom the boolean value to set inBossRoom to
+     */
     public void setInBossRoom(boolean inBossRoom) {
         this.inBossRoom = inBossRoom;
     }
 
+    /**
+     * Uses the static LOWESTX, LOWESTY, HIGHESTX, AND HIGHESTY values to generate random
+     * tile coordinate between the min and max values.
+     * @return an int array. Index 0 is the x-coordinate, index 1 is the y-coordinate
+     */
     private int[] getRandomTileCoordinates () {
         spawnX =  parentRoom.getWorldX() + ((LOWESTX + (int) (Math.random() * ((HIGHESTX - LOWESTX) + 1))) * GameCanvas.TILESIZE);
         spawnY = parentRoom.getWorldY() + ((LOWESTY + (int) (Math.random() * ((HIGHESTY - LOWESTY) + 1))) * GameCanvas.TILESIZE);
@@ -186,6 +248,11 @@ public class MobSpawner {
         return new int[] {spawnX, spawnY};
     }
 
+    /**
+     * Creates a boss based on level and sets its spawn point to the room's center
+     * @param level the mobspawner's level
+     * @return the created boss instance
+     */
     private Enemy createBoss(int level){
         spawnX = parentRoom.getCenterX();
         spawnY = parentRoom.getCenterY();
@@ -194,6 +261,11 @@ public class MobSpawner {
         return createEnemy(bossType, spawnX, spawnY);
     }
 
+    /**
+     * Creates a non-boss enemy based on the available enemies per level listed in spawnableEnemiesAtLevel
+     * @param level the room's level
+     * @return the created normal enemy with coordinates generated by getRandomTileCoordinates()
+     */
     public Enemy createNormalEnemy(int level){
         int[] spawnCoors = getRandomTileCoordinates();
         spawnX = spawnCoors[0];
@@ -203,6 +275,11 @@ public class MobSpawner {
         return createEnemy(toSpawn, spawnX, spawnY);
     }
 
+    /**
+     * Spawns an enemy into the game by adding it into the ServerMaster
+     * entities ArrayList into the parentRoom
+     * @param enemy
+     */
     public void spawnEnemy(Enemy enemy) {
         spawnedCount++;   
         isSpawning = true;
@@ -212,14 +289,26 @@ public class MobSpawner {
         ServerMaster.getInstance().addEntity(enemy);
     }
 
+    /**
+     * Gets the Room parentRoom where the mobSpawner is located
+     * @return the Room reference held in parentRoom
+     */
     public Room getParentRoom() {
         return parentRoom;
     }
 
+    /**
+     * Sets the value of parentRoom to the passed Room argument
+     * @param parentRoom the Room to set parentRoom to
+     */
     public void setParentRoom(Room parentRoom) {
         this.parentRoom = parentRoom;
     }
 
+    /**
+     * Checks if the boss created by the MobSpawner instance is already killed
+     * @return true if the boss is killed, false otherwise
+     */
     public boolean isBossKilled(){
         if (boss == null) return false;
         return boss.getHitPoints() <= 0;
