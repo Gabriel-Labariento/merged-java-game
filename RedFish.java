43a1b60e7058174bd50e4f78b2adc46658260806
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 23:28:36 +0800

    javadoc redfish

--- RedFish.java
+++ RedFish.java
@@ -3,6 +3,26 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+/**     
+        The RedFish class extends Item. It adds one-fourth of the Player's
+        max health to its currentHealth.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
+
 public class RedFish extends Item {
     public static BufferedImage sprite;
     static {
@@ -13,6 +33,11 @@ public class RedFish extends Item {
         }
     }
     
+    /**
+     * Creates a RedFish instance with appropriate fields
+     * @param x the x-coordinate
+     * @param y the y-coordinate
+     */
     public RedFish(int x, int y){
         identifier = NetworkProtocol.REDFISH;
         worldX = x;
