02d7721ef4e9b48f497bc60d8b95a9e073bc82a0
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 23:02:01 +0800

    javadoc poison effect

--- PoisonEffect.java
+++ PoisonEffect.java
@@ -1,9 +1,32 @@
 public class PoisonEffect extends StatusEffect{
 
+    /**     
+        The PoisonEffect class extends StatusEffect. It damages the Player
+        for by subtracting 1 from its hitPoints every second for three
+        seconds.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
     public static final int DPS = 1;
     private static final int DAMAGE_INTERVAL = 1000;
     private long lastAppliedTime = 0;
 
+    /**
+     * Creates a new PoisonEffect instance  with a duration of 3 seconds.
+     */
     public PoisonEffect(){
         duration = 3000;
         expireTime = System.currentTimeMillis() + duration;
