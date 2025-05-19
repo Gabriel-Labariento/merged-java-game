17b1121a6f15a236ae1523bc0dae8663032c404f
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-19 11:28:04 +0800

    Javadoc Item

--- Item.java
+++ Item.java
@@ -1,3 +1,23 @@
+/**     
+        The Item class is an abstract class that extends Entity.
+        It supports item expiration handling, item to player interaction,
+        and item ownership.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 public abstract class Item extends Entity{
     public long despawnTime;
     public long pickUpCDTime;
@@ -14,40 +34,76 @@ public abstract class Item extends Entity{
     public int initialDefense;
     public int initialAttackFrameDuration;
 
+    /**
+     * Upon item creation, triggerDespawnTimer is called immediately
+     * to signal when the item should despawn.
+     */
     public Item(){
         triggerDespawnTimer();
         width = 16;
         height = 16;
     }
 
+    /**
+     * Sets the value of the owner field to the passed argument
+     * @param player the Player to set owner to
+     */
     public void setOwner(Player player){
         owner = player;
     }
 
+    /**
+     * Sets despawnTime to the time of invoction + DROPDURATION
+     */
     public void triggerDespawnTimer(){
         despawnTime = System.currentTimeMillis() + DROPDURATION;
     }
 
+    /**
+     * Sets the valud of the isHeld field
+     * @param b the boolean value to set isHeld to
+     */
     public void setIsHeld(boolean b){
         isHeld = b;
     }
 
+    /**
+     * Applies the item's effect to the owner
+     */
     public abstract void applyEffects();
 
+    /**
+     * Removes the item's effect on the owner
+     */
     public abstract void removeEffects();
 
+    /**
+     * Sets pickUpCDTime to the time of invocation + PICKUPCDDURATION
+     */
     public void triggerPickUpCD(){
         pickUpCDTime = System.currentTimeMillis() + PICKUPCDDURATION;
     }
 
+    /**
+     * Checks whether an item is on pickup cooldown
+     * @return true if the current time is less than the assigned pickUPCDTime, false otherwise
+     */
     public boolean getIsOnPickUpCD(){
         return System.currentTimeMillis() < pickUpCDTime;
     }
 
+    /**
+     * Gets the value of the isConsumable field
+     * @return true if the item is consumable, false otherwise
+     */
     public boolean getIsConsumable(){
         return isConsumable;
     }
 
+    /**
+     * Gets the field value of isDespawned
+     * @return false if the item is held or the current time is less than or equal to despawnTime, true otherwise
+     */
     public boolean getIsDespawned(){
         if (isHeld) return false;
         return System.currentTimeMillis() >= despawnTime;
@@ -79,6 +135,4 @@ public abstract class Item extends Entity{
     @Override
     public void updateEntity(ServerMaster gsm){
     }
-
-
 }
