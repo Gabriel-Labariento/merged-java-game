cdc58eec3c773f370a826f57364038efeb09a4ca
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2025-05-20 08:59:38 +0800

    javadoc slowEffect

--- SlowEffect.java
+++ SlowEffect.java
@@ -1,5 +1,28 @@
+/**     
+        The SlowEffect extends StatusEffect. It lasts for three seconds.
+        It slows down the player by two movement units during the time 
+        of application.
+
+        @author Niles Tristan Cabrera (240828)
+        @author Gabriel Matthew Labariento (242425)
+        @version 20 May 2025
+
+        We have not discussed the Java language code in our program
+        with anyone other than my instructor or the teaching assistants
+        assigned to this course.
+        We have not used Java language code obtained from another student,
+        or any other unauthorized source, either modified or unmodified.
+        If any Java language code or documentation used in our program
+        was obtained from another source, such as a textbook or website,
+        that has been clearly noted with a proper citation in the comments
+        of our program.
+**/
+
 public class SlowEffect extends StatusEffect{
 
+    /**
+     * Creates a SlowEffect instance that lasts for 3 seconds
+     */
     public SlowEffect(){
         duration = 3000;
         expireTime = System.currentTimeMillis() + duration;
@@ -21,8 +44,6 @@ public class SlowEffect extends StatusEffect{
     @Override
     public void removeStatusEffect(Player player) {
         player.setSpeed(player.getBaseSpeed());
-        System.out.println("Removed slow");
-        System.out.println("Player speed: " + player.getSpeed());
     }
 
     @Override
